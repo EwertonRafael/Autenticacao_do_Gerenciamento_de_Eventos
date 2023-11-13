968ee86c4d783f98fa502c31a31f17be7f105d0e
Ewerton Rafael <ton_rafael@hotmail.com>
2023-11-13 19:36:40 -0300

    autenticação e criação do login e autorização

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -21,3 +21,22 @@ export const validationUserFields = async (req: Request, res: Response, next: Ne
         res.status(500).json({ mensagem: "Server error when registering" });
     }
 };
+export const validationLoginFields = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ mensagem: "email and password are mandatory" });
+        }
+        const checkedEmail = await prismaClient.users.findUnique({
+            where: {
+                email
+            }
+        })
+        if (!checkedEmail) {
+            return res.status(400).json({ message: "incorrect email" });
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({ mensagem: "Server error when logging in" });
+    }
+};
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { validationUserFields } from "../middlewares/validation";
-import { createUser } from "../controllers/user";
+import { validationLoginFields, validationUserFields } from "../middlewares/validation";
+import { LoginUser, createUser } from "../controllers/user";
+import { auth } from "../auth/authorization";
 const router = express.Router();
 
 const user = new createUser();
+const login = new LoginUser();
 
 router.post('/register', validationUserFields, user.handle)
+router.post('/login', validationLoginFields, login.handle)
+
+router.use(auth)
+
 
 export = router;
